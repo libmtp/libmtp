a094e136117d4d138d134a607dcae91f5e9dc4ec
Linus Walleij <triad@df.lth.se>
2006-02-15 14:22:08 +0000

    Autogen fixup.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,45 @@
 #!/bin/sh
-set -e
+#set -e
 
-# Refresh GNU autotools toolchain.
-libtoolize --copy --force
-aclocal
-autoheader
-automake --add-missing
+srcdir=`dirname $0`
+
+ACLOCAL_FLAGS="-I ${srcdir}/m4 ${ACLOCAL_FLAGS}"
+
+fail() {
+    status=$?
+    echo "Last command failed with status $status in directory $(pwd)."
+    echo "Aborting"
+    exit $status
+}
+
+# Refresh GNU autotools toolchain: libtool
+echo "Removing libtool cruft"
+rm -f ltmain.sh config.guess config.sub
+echo "Running libtoolize"
+libtoolize --copy --force || fail
+
+# Refresh GNU autotools toolchain: aclocal autoheader
+echo "Removing aclocal cruft"
+rm -f aclocal.m4
+echo "Running aclocal $ACLOCAL_FLAGS"
+aclocal $ACLOCAL_FLAGS || fail
+echo "Removing autoheader cruft"
+rm -f config.h.in src/config.h.in
+echo "Running autoheader"
+autoheader || fail
+
+# Refresh GNU autotools toolchain: automake
+echo "Removing automake cruft"
+rm -f depcomp install-sh missing mkinstalldirs
+rm -f stamp-h*
+echo "Running automake"
+automake --add-missing --gnu || fail
+
+# Refresh GNU autotools toolchain: autoconf
+echo "Removing autoconf cruft"
+rm -f configure
+rm -rf autom4te*.cache/
+echo "Running autoconf"
 autoconf
 
 # Autoupdate config.sub and config.guess 
@@ -23,3 +57,6 @@ if [ "x$WGET" != "x" ]; then
 else
     echo "Could not autoupdate config.sub and config.guess"
 fi
+
+echo "Finished!"
+
