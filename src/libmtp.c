394bbbedcb2cf19fe444aa57933c8fd34b9c2938
Linus Walleij <triad@df.lth.se>
2006-02-22 16:10:53 +0000

    New functionality...

--- src/libmtp.c
+++ src/libmtp.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include "libmtp.h"
 #include "unicode.h"
+#include "ptp.h"
+#include "ptp-pack.h"
+#include "libptp-endian.h"
 
 /**
  * Initialize the library.
@@ -136,7 +139,24 @@ char *LIBMTP_Get_Ownername(LIBMTP_mtpdevice_t *device)
   return retstring;
 }
 
-static LIBMTP_track_t *LIBMTP_new_track_t()
+/**
+ * This creates a new track metadata structure and allocates memory
+ * for it. Notice that if you add strings to this structure they
+ * will be freed by the corresponding <code>LIBMTP_destroy_track_t</code>
+ * operation later, so be careful of using strdup() when assigning 
+ * strings, e.g.:
+ *
+ * <code>
+ * LIBMTP_track_t *track = LIBMTP_new_track_t();
+ * track->title = strdup(titlestr);
+ * ....
+ * LIBMTP_destroy_track_t(track);
+ * </code>
+ *
+ * @return a pointer to the newly allocated metadata structure.
+ * @see LIBMTP_destroy_track_t()
+ */
+LIBMTP_track_t *LIBMTP_new_track_t(void)
 {
   LIBMTP_track_t *new = (LIBMTP_track_t *) malloc(sizeof(LIBMTP_track_t));
   if (new == NULL) {
@@ -156,6 +176,13 @@ static LIBMTP_track_t *LIBMTP_new_track_t()
   return new;
 }
 
+/**
+ * This destroys a track metadata structure and deallocates the memory
+ * used by it, including any strings. Never use a track metadata 
+ * structure again after calling this function on it.
+ * @param track the track metadata to destroy.
+ * @see LIBMTP_new_track_t()
+ */
 void LIBMTP_destroy_track_t(LIBMTP_track_t *track)
 {
   if (track == NULL) {
@@ -371,7 +398,7 @@ int LIBMTP_Get_Track_To_File(LIBMTP_mtpdevice_t *device, uint32_t const id,
 
   // Sanity check
   if (path == NULL) {
-    printf("LIBMTP_Get_Track_To_File(): Bad arguments\n");
+    printf("LIBMTP_Get_Track_To_File(): Bad arguments, path was NULL\n");
     return -1;
   }
 
@@ -422,7 +449,7 @@ int LIBMTP_Get_Track_To_File_Descriptor(LIBMTP_mtpdevice_t *device,
   
   // Not yet compatible.
   globalCallback = NULL;
- 
+
   if (ptp_getobjectinfo(device->params, id, &oi) != PTP_RC_OK) {
     printf("LIBMTP_Get_Track_To_File_Descriptor(): Could not get object info\n");
     return -1;
@@ -456,5 +483,209 @@ int LIBMTP_Get_Track_To_File_Descriptor(LIBMTP_mtpdevice_t *device,
     return -1;
   }
 
+  return 0;
+}
+
+/**
+ * This function sends a track from a local file to an
+ * MTP device. A filename and a set of metadata must be
+ * given as input.
+ * @param device a pointer to the device to send the track to.
+ * @param path the filename of a local file which will be sent.
+ * @param metadata a track metadata set to be written along with the file.
+ * @param callback a progress indicator function or NULL to ignore.
+ * @param data a user-defined pointer that is passed along to
+ *             the <code>progress</code> function in order to
+ *             pass along some user defined data to the progress
+ *             updates. If not used, set this to NULL.
+ * @return 0 if the transfer was successful, any other value means 
+ *           failure.
+ * @see LIBMTP_Send_Track_From_File_Descriptor()
+ */
+int LIBMTP_Send_Track_From_File(LIBMTP_mtpdevice_t *device, 
+			 char const * const path, LIBMTP_track_t * const metadata,
+                         LIBMTP_progressfunc_t const * const callback,
+			 void const * const data)
+{
+  int fd;
+  int ret;
+
+  // Sanity check
+  if (path == NULL) {
+    printf("LIBMTP_Send_Track_From_File(): Bad arguments, path was NULL\n");
+    return -1;
+  }
+
+  // Open file
+#ifdef __WIN32__
+  if ( (fd = open(path, O_RDONLY|O_BINARY) == -1 ) {
+#else
+  if ( (fd = open(path, O_RDONLY)) == -1) {
+#endif
+    printf("LIBMTP_Get_Track_To_File(): Could not open source file \"%s\"\n", path);
+    return -1;
+  }
+
+  ret = LIBMTP_Send_Track_From_File_Descriptor(device, fd, metadata, callback, data);
+  
+  // Close file.
+  close(fd);
+
+  return ret;
+}
+
+/**
+ * This function sends a track from a file descriptor to an
+ * MTP device. A filename and a set of metadata must be
+ * given as input.
+ * @param device a pointer to the device to send the track to.
+ * @param fd the filedescriptor for a local file which will be sent.
+ * @param metadata a track metadata set to be written along with the file.
+ *                 After this call the field <code>item_id</code>
+ *                 will contain the new track ID.
+ * @param callback a progress indicator function or NULL to ignore.
+ * @param data a user-defined pointer that is passed along to
+ *             the <code>progress</code> function in order to
+ *             pass along some user defined data to the progress
+ *             updates. If not used, set this to NULL.
+ * @return 0 if the transfer was successful, any other value means 
+ *           failure.
+ * @see LIBMTP_Send_Track_From_File()
+ */
+int LIBMTP_Send_Track_From_File_Descriptor(LIBMTP_mtpdevice_t *device, 
+			 int const fd, LIBMTP_track_t * const metadata,
+                         LIBMTP_progressfunc_t const * const callback,
+			 void const * const data)
+{
+  uint32_t parenthandle = 0;
+  uint16_t ret;
+  uint32_t store = 0;
+  uint8_t *buffer;
+  uint64_t remain;
+  PTPObjectInfo new_track;
+  PTPContainer ptp;
+  PTPUSBBulkContainerSend usbdata;
+  
+  switch (metadata->codec) {
+  case LIBMTP_CODEC_WAV:
+    new_track.ObjectFormat = PTP_OFC_WAV;
+    break;
+  case LIBMTP_CODEC_MP3:
+    new_track.ObjectFormat = PTP_OFC_MP3;
+    break;
+  case LIBMTP_CODEC_WMA:
+    new_track.ObjectFormat = PTP_OFC_WMA;
+    break;
+  default:
+    printf("LIBMTP_Send_Track_From_File_Descriptor: unknown codec.\n");
+    new_track.ObjectFormat = PTP_OFC_Undefined;
+  }
+
+  new_track.Filename = metadata->filename;
+  new_track.ObjectCompressedSize = metadata->filesize;
+
+  // Create the object
+  ret = ptp_sendobjectinfo(device->params, &store, &parenthandle, &metadata->item_id, &new_track);
+  if (ret != PTP_RC_OK) {
+    ptp_perror(device->params, ret);
+    printf("LIBMTP_Send_Track_From_File_Descriptor: Could not send object info\n");
+    return -1;
+  }
+
+  // Nullify and configure PTP container
+  memset(&ptp, 0, sizeof(ptp));
+  ptp.Code = PTP_OC_SendObject;
+  ptp.Nparam = 0;
+  ptp.Transaction_ID = device->params->transaction_id++;
+  ptp.SessionID = device->params->session_id;
+  // Send request to send an object
+  ret = device->params->sendreq_func(device->params, &ptp);
+  if (ret != PTP_RC_OK) {
+    ptp_perror(device->params, ret);
+    printf("LIBMTP_Send_Track_From_File_Descriptor: Could not send \"send object\" request\n");
+    return -1;
+  }
+  
+  // build appropriate USB container
+  usbdata.length = htod32p(device->params,sizeof(usbdata) + metadata->filesize);
+  usbdata.type = htod16p(device->params,PTP_USB_CONTAINER_DATA);
+  usbdata.code = htod16p(device->params,PTP_OC_SendObject);
+  usbdata.trans_id = htod32p(device->params,ptp.Transaction_ID);
+  // Write request to USB
+  ret = device->params->write_func((unsigned char *)&usbdata, sizeof(usbdata), device->params->data);
+  if (ret != PTP_RC_OK) {
+    printf("LIBMTP_Send_Track_From_File_Descriptor: Error initializing object sending\n");
+    ptp_perror(device->params, ret);
+    return -1;
+  }
+	
+  // This space will be used as a reading ring buffer for the transfers
+  buffer = (uint8_t *) malloc(BLOCK_SIZE);
+  if (buffer == NULL) {
+    printf("LIBMTP_Send_Track_From_File_Descriptor: Could not allocate send buffer\n");
+    return -1;
+  }
+	
+  remain = metadata->filesize;
+  while (remain != 0) {
+    int readsize = (remain > BLOCK_SIZE) ? BLOCK_SIZE : (int) remain;
+    int bytesdone = (int) (metadata->filesize - remain);
+    
+    if (read(fd, buffer, readsize) < readsize) {
+      printf("LIBMTP_Send_Track_From_File_Descriptor: error reading source file\n");
+      free(buffer);
+      return -1;
+    }
+    
+    if (callback != NULL) {
+      // If the callback return anything else than 0, interrupt the processing
+      int callret = callback(bytesdone, metadata->filesize, buffer, readsize, data);
+      if (callret != 0) {
+	printf("LIBMTP_Send_Track_From_File_Descriptor: transfer interrupted by callback\n");
+	free(buffer);
+	return -1;
+      }
+    }
+    
+    // Write to USB
+    ret = device->params->write_func(buffer, readsize, device->params->data);
+    if (ret != PTP_RC_OK) {
+      printf("LIBMTP_Send_Track_From_File_Descriptor: error writing data chunk to object\n");
+      ptp_perror(device->params, ret);
+      free(buffer);
+      return -1;
+    }
+    remain -= BLOCK_SIZE;
+  }
+  
+  if (callback != NULL) {
+    // This last call will not be able to abort execution and is just
+    // done do progress bars go up to 100%
+    (void) callback(metadata->filesize, metadata->filesize, NULL, 0, data);
+  }
+  
+  // Signal to USB that this is the last transfer if the last chunk
+  // was exactly as large as the buffer
+  if (metadata->filesize % MTP_DEVICE_BUF_SIZE == 0) {
+    ret = device->params->write_func(NULL, 0, device->params->data);
+    if (ret!=PTP_RC_OK) {
+      printf("LIBMTP_Send_Track_From_File_Descriptor: error writing last zerolen data chunk for USB termination\n");
+      ptp_perror(device->params, ret);
+      free(buffer);
+      return -1;
+    }
+  }
+
+  // Get a response from device to make sure that the track was properly stored
+  ret = device->params->getresp_func(device->params, &ptp);
+  if (ret!=PTP_RC_OK) {
+    printf("LIBMTP_Send_Track_From_File_Descriptor: error getting response from device\n");
+    ptp_perror(device->params, ret);
+    free(buffer);
+    return -1;
+  }
+  
+  // Free allocated buffer and return
+  free(buffer);
   return 0;
 }
