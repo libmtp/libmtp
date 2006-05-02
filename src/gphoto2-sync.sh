f9e1e13ff41c486b029013c70661d2339edd0fcb
Linus Walleij <triad@df.lth.se>
2006-05-02 10:43:44 +0000

    Fix up CVS

---
+++ src/gphoto2-sync.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#set -e
+
+srcdir=`dirname $0`
+
+# Get sources from gphoto2 SVN
+WGET=`which wget`
+if [ "x$WGET" != "x" ]; then
+    wget -O tmpfile http://svn.sourceforge.net/viewcvs.cgi/*checkout*/gphoto/trunk/libgphoto2/camlibs/ptp2/ptp.c
+    mv tmpfile ptp.c.gphoto2
+    wget -O tmpfile http://svn.sourceforge.net/viewcvs.cgi/*checkout*/gphoto/trunk/libgphoto2/camlibs/ptp2/ptp.h
+    mv tmpfile ptp.h.gphoto2
+    wget -O tmpfile http://svn.sourceforge.net/viewcvs.cgi/*checkout*/gphoto/trunk/libgphoto2/camlibs/ptp2/ptp-pack.c
+    mv tmpfile ptp-pack.c.gphoto2
+    wget -O tmpfile http://svn.sourceforge.net/viewcvs.cgi/*checkout*/gphoto/trunk/libgphoto2/camlibs/ptp2/library.c
+    mv tmpfile library.c.gphoto2
+else
+    echo "Could not sync to gphoto2. No WGET."
+fi
+
+echo "Finished!"
+
