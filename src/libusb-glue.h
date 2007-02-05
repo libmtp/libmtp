7b7a0e2b9d465dad85dca593693280f1df79a74d
Linus Walleij <triad@df.lth.se>
2007-02-05 18:22:27 +0000

    LGPL boilerplates.

--- src/libusb-glue.h
+++ src/libusb-glue.h
@@ -1,8 +1,29 @@
-/*
- *  libusb-glue.h
+/**
+ * \file libusb-glue.h
+ * Low-level USB interface glue towards libusb.
  *
- *  Created by Richard Low on 24/12/2005.
- *  Modified by Linus Walleij
+ * Copyright (C) 2005-2007 Richard A. Low <richard@wentnet.com>
+ * Copyright (C) 2005-2007 Linus Walleij <triad@df.lth.se>
+ * Copyright (C) 2006-2007 Marcus Meissner
+ * Copyright (C) 2007 Ted Bullock
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Created by Richard Low on 24/12/2005.
+ * Modified by Linus Walleij
  *
  */
 
