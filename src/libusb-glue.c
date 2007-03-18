6c8fea4cd63536d1630aaf18959a556062b9877f
Richard Low <richard@wentnet.com>
2007-03-18 19:11:52 +0000

    New devices

--- src/libusb-glue.c
+++ src/libusb-glue.c
@@ -114,7 +114,7 @@ static const LIBMTP_device_entry_t mtp_device_table[] = {
   // Reported by Andrew Benson
   { "Samsung YP-F2J", 0x04e8, 0x5057, DEVICE_FLAG_DUALMODE },
   // Reported by Patrick <skibler@gmail.com>
-  { "Samsung YP-K5", 0x04e8, 0x505a, DEVICE_FLAG_NONE },
+  { "Samsung YP-K5", 0x04e8, 0x505a, DEVICE_FLAG_NO_ZERO_READS },
   // Reported by Matthew Wilcox <matthew@wil.cx>
   { "Samsung YP-T9", 0x04e8, 0x507f, DEVICE_FLAG_NONE },
   // From Paul Clinch
