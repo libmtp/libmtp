8282c97b7e7a2d3d83f313ed48e23d0067e0ce01
Linus Walleij <triad@df.lth.se>
2007-04-10 19:59:16 +0000

    Archos 504

--- src/libusb-glue.c
+++ src/libusb-glue.c
@@ -252,6 +252,8 @@ static const LIBMTP_device_entry_t mtp_device_table[] = {
   { "Archos 104 (MTP mode)", 0x0e79, 0x120a, DEVICE_FLAG_NONE },
   // Added by Jan Binder
   { "Archos XS202 (MTP mode)", 0x0e79, 0x1208, DEVICE_FLAG_NONE },
+  // Reported by Etienne Chauchot <chauchot.etienne@free.fr>
+  { "Archos 504 (MTP mode)", 0x0e79, 0x1307, DEVICE_FLAG_NONE },
 
   /*
    * Dunlop (OEM of EGOMAN ltd?) reported by Nanomad
