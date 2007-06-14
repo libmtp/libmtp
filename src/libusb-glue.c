0f8a6f55657278dc651accbc4ec92c7fe6302584
Linus Walleij <triad@df.lth.se>
2007-06-14 08:37:58 +0000

    A new Nokia device...

--- src/libusb-glue.c
+++ src/libusb-glue.c
@@ -306,7 +306,9 @@ static const LIBMTP_device_entry_t mtp_device_table[] = {
   /*
    * Nokia
    */
-  {"Nokia Mobile Phones (MTP mode)", 0x0421, 0x04e1, DEVICE_FLAG_NONE },
+  {"Some Nokia Mobile Phone (MTP mode)", 0x0421, 0x04e1, DEVICE_FLAG_NONE },
+  // From: Pat Nicholls <pat@patandannie.co.uk>
+  {"Nokia N80 Internet Edition (Media Player)", 0x0421, 0x04f1, DEVICE_FLAG_UNLOAD_DRIVER },
 
   /*
    * LOGIK
