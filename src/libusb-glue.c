d6fcaa92e0762ccd4c23e02cb6220ccf6d82297c
Linus Walleij <triad@df.lth.se>
2007-09-02 06:59:13 +0000

    Cowon D2

--- src/libusb-glue.c
+++ src/libusb-glue.c
@@ -404,7 +404,7 @@ static const LIBMTP_device_entry_t mtp_device_table[] = {
   // Reported by Roberth Karman
   {"Cowon iAudio 7 (MTP mode)", 0x0e21, 0x0751, DEVICE_FLAG_NONE },
   // Reported by TJ Something <tjbk_tjb@users.sourceforge.net>
-  {"Cowon iAudio D2 (MTP mode)", 0x0e21, 0x0801, DEVICE_FLAG_NONE },
+  {"Cowon iAudio D2 (MTP mode)", 0x0e21, 0x0801, DEVICE_FLAG_UNLOAD_DRIVER },
 
   /*
    * Insignia, dual-mode.
