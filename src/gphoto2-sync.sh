4cfbaa12cb8e1db1a82e10c397c231c162107a5c
Linus Walleij <triad@df.lth.se>
2013-11-04 01:49:35 +0100

    fix fetch script
    Signed-off-by: Linus Walleij <triad@df.lth.se>

--- src/gphoto2-sync.sh
+++ src/gphoto2-sync.sh
@@ -2,19 +2,21 @@
 #set -e
 
 srcdir=`dirname $0`
+BASEURL="http://sourceforge.net/p/gphoto/code/14453/tree/trunk/libgphoto2/camlibs/ptp2/"
+#BASEURL="http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/"
 
 # Get sources from gphoto2 SVN
 WGET=`which wget`
 if [ "x$WGET" != "x" ]; then
-    wget -O tmpfile http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/ptp.c?format=raw
+    wget -O tmpfile $BASEURL/ptp.c?format=raw
     mv tmpfile ptp.c.gphoto2
-    wget -O tmpfile tmpfile http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/ptp.h?format=raw
+    wget -O tmpfile tmpfile $BASEURL/ptp.h?format=raw
     mv tmpfile ptp.h.gphoto2
-    wget -O tmpfile tmpfile http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/ptp-pack.c?format=raw
+    wget -O tmpfile tmpfile $BASEURL/ptp-pack.c?format=raw
     mv tmpfile ptp-pack.c.gphoto2
-    wget -O tmpfile tmpfile http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/library.c?format=raw
+    wget -O tmpfile tmpfile $BASEURL/library.c?format=raw
     mv tmpfile library.c.gphoto2
-    wget -O tmpfile tmpfile http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/music-players.h?format=raw
+    wget -O tmpfile tmpfile $BASEURL/music-players.h?format=raw
     mv tmpfile music-players.h.gphoto2
 else
     echo "Could not sync to gphoto2. No WGET."
