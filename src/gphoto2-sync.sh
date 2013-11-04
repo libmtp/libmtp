566f0769bf5f3c989c04f42c8b6767c3bb192b28
Linus Walleij <triad@df.lth.se>
2013-11-04 02:04:12 +0100

    fix the sync script to use svn
    Signed-off-by: Linus Walleij <triad@df.lth.se>

--- src/gphoto2-sync.sh
+++ src/gphoto2-sync.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 #set -e
 
-srcdir=`dirname $0`
-BASEURL="http://sourceforge.net/p/gphoto/code/14453/tree/trunk/libgphoto2/camlibs/ptp2/"
-#BASEURL="http://sourceforge.net/p/gphoto/code/14266/tree/trunk/libgphoto2/camlibs/ptp2/"
-
-# Get sources from gphoto2 SVN
-WGET=`which wget`
-if [ "x$WGET" != "x" ]; then
-    wget -O tmpfile $BASEURL/ptp.c?format=raw
-    mv tmpfile ptp.c.gphoto2
-    wget -O tmpfile tmpfile $BASEURL/ptp.h?format=raw
-    mv tmpfile ptp.h.gphoto2
-    wget -O tmpfile tmpfile $BASEURL/ptp-pack.c?format=raw
-    mv tmpfile ptp-pack.c.gphoto2
-    wget -O tmpfile tmpfile $BASEURL/library.c?format=raw
-    mv tmpfile library.c.gphoto2
-    wget -O tmpfile tmpfile $BASEURL/device-flags.h?format=raw
-    mv tmpfile device-flags.h.gphoto2
-    wget -O tmpfile tmpfile $BASEURL/music-players.h?format=raw
-    mv tmpfile music-players.h.gphoto2
-else
-    echo "Could not sync to gphoto2. No WGET."
+if [ ! -d ptp2 ] ; then
+    echo "No copy of the gphoto trunk, checking out..."
+    svn checkout svn://svn.code.sf.net/p/gphoto/code/trunk/libgphoto2/camlibs/ptp2 ptp2
+fi
+if [ ! -d ptp2 ] ; then
+    echo "Could not clone gphoto trunk."
+    exit 1
 fi
 
-echo "Finished!"
+cd ptp2
+svn update
+cd ..
 
+cp ptp2/ptp.c ptp.c
+cp ptp2/ptp.h ptp.h
+cp ptp2/ptp-pack.c ptp-pack.c
+
+echo "Finished!"
