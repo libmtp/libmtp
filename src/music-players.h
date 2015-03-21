96bab8877ca735bf8e5c8b81348d92881a95f847
Marcus Meissner <marcus@jet.franken.de>
2015-03-21 10:24:33 +0100

    https://sourceforge.net/p/libmtp/bugs/1243/
    Prestigio PSP5504DUO

--- src/music-players.h
+++ src/music-players.h
@@ -2634,6 +2634,8 @@
 
   /* https://sourceforge.net/p/libmtp/bugs/1292/ */
   { "Prestigio", 0x29e4, "5505 DUO ", 0x1103, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1243/ */
+  { "Prestigio", 0x29e4, "5504 DUO ", 0x1203, DEVICE_FLAGS_ANDROID_BUGS },
 
   /* https://sourceforge.net/p/libmtp/bugs/1283/ */
   { "Megafon", 0x201e, "MFLogin3T", 0x42ab, DEVICE_FLAGS_ANDROID_BUGS },
