dc714ac741722193ccd17e07b77639437d448ef5
Marcus Meissner <marcus@jet.franken.de>
2015-03-21 12:33:14 +0100

    https://sourceforge.net/p/libmtp/bugs/1298/
    acer z160

--- src/music-players.h
+++ src/music-players.h
@@ -581,6 +581,9 @@
       DEVICE_FLAGS_ANDROID_BUGS },
   { "Acer", 0x0502, "Liquid Z130 MT65xx Android Phone", 0x355f,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1298/ */
+  { "Acer", 0x0502, "Z160", 0x361d,
+      DEVICE_FLAGS_ANDROID_BUGS },
   { "Acer", 0x0502, "Iconia A1-840FHD", 0x362d,
       DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1251/ */
