c15dd5d5a24a1b7450033c1574810bed0b669e6a
Marcus Meissner <marcus@jet.franken.de>
2015-03-21 10:35:19 +0100

    https://sourceforge.net/p/libmtp/bugs/1245/
    DigiLand
    DL701Q

--- src/music-players.h
+++ src/music-players.h
@@ -2624,6 +2624,9 @@
    */
   { "Kurio", 0x1f3a, "7S", 0x1006,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1245/ */
+  { "DigiLand", 0x1f3a, "DL701Q", 0x0c02,
+      DEVICE_FLAGS_ANDROID_BUGS },
 
   /* In update 4 the order of devices was changed for
      better OS X / Windows suport and another device-id
