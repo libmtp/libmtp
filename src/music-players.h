987724446e9d20b6c951e5cfecffab1e5ed01de1
Marcus Meissner <marcus@jet.franken.de>
2015-03-22 22:44:18 +0100

    https://sourceforge.net/p/libmtp/bugs/1095/
    another hongmi id, also insight into ADB id relation

--- src/music-players.h
+++ src/music-players.h
@@ -2626,7 +2626,10 @@
   /* https://sourceforge.net/p/libmtp/bugs/1149/ */
   { "Xiaomi", 0x2717, "MiPad (MTP)", 0x0660,
       DEVICE_FLAGS_ANDROID_BUGS },
-  { "Xiaomi", 0x2717, "Hongmi (MTP)", 0x1240,
+  { "Xiaomi", 0x2717, "Hongmi (MTP+ADB)", 0x1240,
+      DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1095/ */
+  { "Xiaomi", 0x2717, "Hongmi (MTP)", 0x1248,
       DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1295/ */
   { "Redmi", 0x2717, "1S (MTP)", 0x1260,
