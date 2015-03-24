b02baf65456f017d4d7e97c1186b179cb2ac2fec
Marcus Meissner <marcus@jet.franken.de>
2015-03-24 09:50:38 +0100

    https://sourceforge.net/p/libmtp/bugs/1155/
    Lenovo Yoga Tab 10 B8000-H

--- src/music-players.h
+++ src/music-players.h
@@ -2409,6 +2409,9 @@
       DEVICE_FLAGS_ANDROID_BUGS },
   { "Lenovo", 0x17ef, "IdeaTab S5000", 0x76e8,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1155/ */
+  { "Lenovo", 0x17ef, "Yoga Tablet 10 B8000-H", 0x76ff,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1291/ */
   { "Lenovo", 0x17ef, "A3500-F", 0x7737,
       DEVICE_FLAGS_ANDROID_BUGS },
