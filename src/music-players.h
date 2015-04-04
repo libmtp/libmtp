fce52e927d4e02e14700bed799ec0ebc69fd9925
Marcus Meissner <marcus@linux-n4oc.site>
2015-04-04 18:28:37 +0200

    https://sourceforge.net/p/libmtp/bugs/1267/ yota phone 2

--- src/music-players.h
+++ src/music-players.h
@@ -2891,6 +2891,8 @@
 
   /* https://sourceforge.net/p/libmtp/bugs/1229/ */
   { "Yota", 0x2916, "Phone 2", 0x914d, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1267/ */
+  { "Yota", 0x2916, "Phone 2 (ID2)", 0xf003, DEVICE_FLAGS_ANDROID_BUGS },
 
   /* https://sourceforge.net/p/libmtp/bugs/1212/ */
   { "Fly", 0x2970, "Evo Tech 4", 0x2008, DEVICE_FLAGS_ANDROID_BUGS },
