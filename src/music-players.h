a480059876f785ff83c05cc73f0911479f2f2689
Marcus Meissner <marcus@jet.franken.de>
2015-06-30 09:12:54 +0200

    Kyocera Torque Model E6715 https://sourceforge.net/p/libmtp/feature-requests/134/

--- src/music-players.h
+++ src/music-players.h
@@ -2846,6 +2846,8 @@
    * Kyocera
    */
   { "Kyocera", 0x0482, "Rise", 0x0571, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/feature-requests/134/ */
+  { "Kyocera", 0x0482, "Torque Model E6715", 0x0059a, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/discussion/535190/thread/6270f5ce/ */
   { "Kyocera", 0x0482, "KYL22", 0x0810, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1345/ */
