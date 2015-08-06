2db72f99515ee1d5a4843a399e581fd3148acf1d
Marcus Meissner <marcus@jet.franken.de>
2015-08-06 09:36:05 +0200

    added acer z200 https://sourceforge.net/p/libmtp/support-requests/183/

--- src/music-players.h
+++ src/music-players.h
@@ -612,6 +612,9 @@
       DEVICE_FLAGS_ANDROID_BUGS },
   { "Acer", 0x0502, "One 7", 0x3657,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/support-requests/183/ */
+  { "Acer", 0x0502, "Z200", 0x3683,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1341/ */
   { "Acer", 0x0502, "Liquid S56", 0x3725,
       DEVICE_FLAGS_ANDROID_BUGS },
