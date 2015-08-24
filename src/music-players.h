282e71b18eafd1041e0ae3966f893ae1e0c5c24a
Marcus Meissner <marcus@jet.franken.de>
2015-08-24 21:43:58 +0200

    lenovo yoga tablet 2 https://sourceforge.net/p/libmtp/support-requests/186/

--- src/music-players.h
+++ src/music-players.h
@@ -2482,6 +2482,9 @@
   /* https://sourceforge.net/p/libmtp/bugs/1291/ */
   { "Lenovo", 0x17ef, "A3500-F", 0x7737,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/support-requests/186/ */
+  { "Lenovo", 0x17ef, "Yoga Tablet 2 - 1050F", 0x77a4,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/support-requests/168/ */
   { "Lenovo", 0x17ef, "Yoga Tablet 2 Pro", 0x77b1,
       DEVICE_FLAGS_ANDROID_BUGS },
