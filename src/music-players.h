dd0435ac131f012a59e5adad7eb7223ad573e6a4
Marcus Meissner <marcus@jet.franken.de>
2015-11-11 20:50:26 +0100

    lenovo a1000 https://sourceforge.net/p/libmtp/bugs/1465/

--- src/music-players.h
+++ src/music-players.h
@@ -2560,6 +2560,9 @@
   /* https://sourceforge.net/p/libmtp/bugs/1360/ */
   { "Lenovo", 0x17ef, "K3 Note", 0x7883,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1465/ */
+  { "Lenovo", 0x17ef, "A1000 Smartphone", 0x7928,
+      DEVICE_FLAGS_ANDROID_BUGS },
 
   /*
    * Huawei
