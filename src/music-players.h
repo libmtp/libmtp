1ec88144ba42e910d1d548c7ce5256c1987c0a06
Marcus Meissner <marcus@jet.franken.de>
2015-11-15 22:01:37 +0100

    ARCHOS Helium 45B

--- src/music-players.h
+++ src/music-players.h
@@ -967,6 +967,8 @@
   { "Archos", 0x0e79, "70it2 (ID 2)", 0x1569, DEVICE_FLAGS_ANDROID_BUGS },
   { "Archos", 0x0e79, "50c", 0x2008, DEVICE_FLAGS_ANDROID_BUGS },
   { "Archos", 0x0e79, "C40", 0x31ab, DEVICE_FLAGS_ANDROID_BUGS },
+  /* via libmtp-discuss Tonton <to.tonton@gmail.com> */
+  { "Archos", 0x0e79, "Helium 45B", 0x31d8, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1393/ */
   { "Archos", 0x0e79, "Phone", 0x31e1, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1325/ */
