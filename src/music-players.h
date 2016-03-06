d1cc261a6fadfc97719fb03e47be68ff7995b4a5
Marcus Meissner <marcus@jet.franken.de>
2016-03-06 22:02:06 +0100

    add cat s50 2nd id

--- src/music-players.h
+++ src/music-players.h
@@ -3197,6 +3197,8 @@
 
   /* https://sourceforge.net/p/libmtp/bugs/1201/ */
   { "Caterpillar", 0x04b7, "Cat S50", 0x88a9, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1525/ */
+  { "Caterpillar", 0x04b7, "Cat S50 (2nd ID)", 0x88aa, DEVICE_FLAGS_ANDROID_BUGS },
 
   /* https://sourceforge.net/p/libmtp/bugs/682/ */
   { "Pegatron", 0x1d4d, "Chagall (ADB)", 0x5035, DEVICE_FLAGS_ANDROID_BUGS },
