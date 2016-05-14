777203683b5866a63722f022f2e697b0c77d2e23
Marcus Meissner <marcus@jet.franken.de>
2016-05-14 16:24:46 +0200

    BQ Aquaris M10 Ubuntu Edition Full HD https://sourceforge.net/p/libmtp/feature-requests/163/

--- src/music-players.h
+++ src/music-players.h
@@ -3207,6 +3207,9 @@
    */
   { "bq", 0x2a47, "Krillin (MTP)", 0x2008,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/feature-requests/163/ */
+  { "bq", 0x2a47, "Aquaris M10 (MTP)", 0x200d,
+      DEVICE_FLAGS_ANDROID_BUGS },
   { "bq", 0x2a47, "Krillin (MTP+ADB)", 0x0c02,
       DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1311/ */
