8440efd14f522e3f7b6f2ee681a94dfc951f5d51
Marcus Meissner <marcus@jet.franken.de>
2016-12-30 23:25:44 +0100

    added tp-link neffos y5 https://sourceforge.net/p/libmtp/feature-requests/195/

--- src/music-players.h
+++ src/music-players.h
@@ -3494,6 +3494,8 @@
   { "TP-Link" , 0x2357, "Neffos C5 MAX (MTP)", 0x031a, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/feature-requests/196/ */
   { "TP-Link" , 0x2357, "Neffos Y5L (MTP)", 0x0320, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/feature-requests/195/ */
+  { "TP-Link" , 0x2357, "Neffos Y5 (MTP)", 0x0328, DEVICE_FLAGS_ANDROID_BUGS },
 
   /* https://sourceforge.net/p/libmtp/bugs/1570/ */
   { "Recon Instruments" , 0x2523, "Jet", 0xd209, DEVICE_FLAGS_ANDROID_BUGS },
