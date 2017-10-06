f29729fe5ebd50cd4ea9c66737b0cacad5888fe7
Marcus Meissner <marcus@jet.franken.de>
2017-10-06 14:29:52 +0200

    inep_maxpacket -> outep_maxpacket

--- src/libopenusb1-glue.c
+++ src/libopenusb1-glue.c
@@ -1213,7 +1213,7 @@ ptp_usb_senddata(PTPParams* params, PTPContainer* ptp,
     unsigned long packet_size;
     PTP_USB *ptp_usb = (PTP_USB *) params->data;
 
-    packet_size = ptp_usb->inep_maxpacket;
+    packet_size = ptp_usb->outep_maxpacket;
 
     LIBMTP_USB_DEBUG("SEND DATA PHASE\n");
 
