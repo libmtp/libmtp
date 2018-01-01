3143fdf7b26c1c0592f6c0d31ec4bbc0139fb232
Marcus Meissner <marcus@jet.franken.de>
2018-01-01 19:26:14 +0100

    added ZTE Blade L3 https://sourceforge.net/p/libmtp/bugs/1745/

--- src/music-players.h
+++ src/music-players.h
@@ -2947,6 +2947,8 @@
   /* https://sourceforge.net/p/libmtp/bugs/672/ */
   { "ZTE", 0x19d2, "Grand X In", 0x0343, DEVICE_FLAGS_ANDROID_BUGS },
   { "ZTE", 0x19d2, "V985", 0x0383, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1745/ */
+  { "ZTE", 0x19d2, "Blade L3", 0x2008, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1328/ */
   { "ZTE", 0x19d2, "V5", 0xffce, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1646/ */
