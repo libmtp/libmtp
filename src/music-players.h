c54f6f15d83faff59991fbdab0f797a72c783c80
Marcus Meissner <marcus@jet.franken.de>
2018-03-18 11:29:19 +0100

    added asus zenfone v https://sourceforge.net/p/libmtp/feature-requests/255/

--- src/music-players.h
+++ src/music-players.h
@@ -2695,6 +2695,9 @@
   /* https://sourceforge.net/p/libmtp/bugs/1364/ */
   { "Asus", 0x0b05, "Zenfone 2 ZE551ML (MTP)", 0x5f03,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/feature-requests/255/ */
+  { "Asus", 0x0b05, "Zenfone V (MTP)", 0x610f,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1642/ */
   { "Asus", 0x0b05, "ME581CL", 0x7770,
       DEVICE_FLAGS_ANDROID_BUGS },
