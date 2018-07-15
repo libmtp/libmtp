20b220b6ad92c4ef0c955edaf63c4bf1ce466760
Marcus Meissner <marcus@jet.franken.de>
2018-07-15 16:57:25 +0200

    added bq aquarius u https://sourceforge.net/p/libmtp/support-requests/269/

--- src/music-players.h
+++ src/music-players.h
@@ -3619,6 +3619,9 @@
   /* https://sourceforge.net/p/libmtp/bugs/1541/ */
   { "bq", 0x2a47, "Aquarius M5.5", 0x901b,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/support-requests/269/ */
+  { "bq", 0x2a47, "Aquarius U", 0x9039,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/feature-requests/231/ */
   { "bq", 0x2a47, "U Plus", 0xf003, DEVICE_FLAGS_ANDROID_BUGS },
 
