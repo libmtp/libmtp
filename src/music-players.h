46fda9f199f5fd6cc56d646327396b4a2a1397b0
Marcus Meissner <meissner@suse.de>
2019-09-29 15:15:04 +0200

    added amazon kindle fire kids https://sourceforge.net/p/libmtp/bugs/1845/

--- src/music-players.h
+++ src/music-players.h
@@ -3401,6 +3401,9 @@
   /* https://sourceforge.net/p/libmtp/bugs/1776/ */
   { "Amazon", 0x1949, "Kindle Fire 7 (2nd ID)", 0x0271,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1845/ */
+  { "Amazon", 0x1949, "Kindle Fire Kids", 0x0272,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://github.com/libmtp/libmtp/issues/21 */
   { "Amazon", 0x1949, "Kindle Fire 8 HD (2nd ID)", 0x0331,
       DEVICE_FLAGS_ANDROID_BUGS },
