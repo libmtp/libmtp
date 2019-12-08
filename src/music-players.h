c262d0d3843b81e770bc7de19a9b8685e5b19f97
Marcus Meissner <marcus@jet.franken.de>
2019-12-08 09:33:04 +0100

    added gopro hero8 black https://sourceforge.net/p/libmtp/bugs/1858/

--- src/music-players.h
+++ src/music-players.h
@@ -3930,6 +3930,7 @@
   { "GoPro" , 0x2672, "FUSION (front)", 0x0035, DEVICE_FLAG_NONE },
   { "GoPro" , 0x2672, "HERO6 Black", 0x0037, DEVICE_FLAG_NONE },
   { "GoPro" , 0x2672, "HERO7 Black", 0x0047, DEVICE_FLAG_NONE },
+  { "GoPro" , 0x2672, "HERO8 Black", 0x0049, DEVICE_FLAG_NONE },
 
   /* https://sourceforge.net/p/libmtp/bugs/1490/ */
   { "Marshall" , 0x2ad9, "London", 0x000b, DEVICE_FLAG_NONE },
