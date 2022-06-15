c729889ad6a68d306716260fc5efeeba07a87e86
Marcus Meissner <marcus@jet.franken.de>
2022-06-15 15:12:24 +0200

    added Acer Liquid Zest T06
    https://sourceforge.net/p/libmtp/support-requests/298/

--- src/music-players.h
+++ src/music-players.h
@@ -644,6 +644,9 @@
   /* https://sourceforge.net/p/libmtp/feature-requests/298/ */
   { "Acer", 0x0502, "A3-A40", 0x387a,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/support-requests/298/ */
+  { "Acer", 0x0502, "Zest T06", 0x3886,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1733/ */
   { "Acer", 0x0502, "Liquid Zest 4G", 0x38a5,
       DEVICE_FLAGS_ANDROID_BUGS },
