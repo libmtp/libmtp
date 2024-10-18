41e2cdc753cec6fcce02ce5753f9fb978012820a
liujinchang <liujinchang@uniontech.com>
2024-10-18 17:58:21 +0800

    add Lenovo Tab Plus
    fixes https://github.com/libmtp/libmtp/issues/223

--- src/music-players.h
+++ src/music-players.h
@@ -3207,6 +3207,9 @@
   /* https://github.com/libmtp/libmtp/issues/173 */
   { "Lenovo", 0x17ef, "TAB P11 Plus", 0x7d4b,
       DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://github.com/libmtp/libmtp/issues/223 */
+  { "Lenovo", 0x17ef, "TAB Plus", 0x7ea5,
+      DEVICE_FLAGS_ANDROID_BUGS },
   /* https://sourceforge.net/p/libmtp/bugs/1945/ */
   { "Lenovo", 0x17ef, "TAB M11", 0x7e7c,
       DEVICE_FLAGS_ANDROID_BUGS },
