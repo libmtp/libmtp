b2821c5d986fa4fc2b8e6ff9fc7bbbd32b2ad88b
Marcus Meissner <marcus@jet.franken.de>
2024-11-19 11:14:31 +0100

    added Garmin Edge 1040 https://sourceforge.net/p/libmtp/bugs/1946/

--- src/music-players.h
+++ src/music-players.h
@@ -4027,6 +4027,8 @@
   { "Garmin", 0x091e, "Venu 2", 0x4e77, DEVICE_FLAGS_ANDROID_BUGS },
   { "Garmin", 0x091e, "Venu 2s", 0x4e78, DEVICE_FLAGS_ANDROID_BUGS },
   { "Garmin", 0x091e, "Venu Mercedes-Benz Collection", 0x4e9C, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://sourceforge.net/p/libmtp/bugs/1946/ */
+  { "Garmin", 0x091e, "Edge 1040", 0x4f03, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://github.com/libmtp/libmtp/issues/161 */
   { "Garmin", 0x091e, "Venu 2 Plus", 0x4f0b, DEVICE_FLAGS_ANDROID_BUGS },
   { "Garmin", 0x091e, "Fenix 7s", 0x4f41, DEVICE_FLAGS_ANDROID_BUGS },
