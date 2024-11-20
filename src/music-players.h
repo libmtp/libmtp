6378b8110f9c5301a6a92c829e3a53a6c402cc03
Marcus Meissner <marcus@jet.franken.de>
2024-11-20 10:42:39 +0100

    added Astell & Kern SE300 https://sourceforge.net/p/libmtp/bugs/1939/

--- src/music-players.h
+++ src/music-players.h
@@ -894,6 +894,10 @@
   { "A&K", 0x4102, "SE180", 0x1230,
     DEVICE_FLAG_BROKEN_MTPGETOBJPROPLIST | DEVICE_FLAG_NO_ZERO_READS |
     DEVICE_FLAG_OGG_IS_UNKNOWN },
+  /* https://sourceforge.net/p/libmtp/bugs/1939/ */
+  { "A&K", 0x4102, "SE300", 0x1249,
+    DEVICE_FLAG_BROKEN_MTPGETOBJPROPLIST | DEVICE_FLAG_NO_ZERO_READS |
+    DEVICE_FLAG_OGG_IS_UNKNOWN },
   // Reported by Scott Call
   // Assume this actually supports OGG though it reports it doesn't.
   { "iRiver", 0x4102, "H10 20GB", 0x2101,
