ed3c72877fc85234da4aaee1a6674f821290b41f
Marcus Meissner <marcus@jet.franken.de>
2024-12-31 10:59:56 +0100

    added Garmin Fenix 8 AMOLED
    fixes https://github.com/libmtp/libmtp/issues/251

--- src/music-players.h
+++ src/music-players.h
@@ -4076,6 +4076,8 @@
   { "Garmin", 0x091e, "Fenix 7s Pro Sapphire Solar", 0x5116, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://github.com/libmtp/libmtp/issues/194 */
   { "Garmin", 0x091e, "Fenix 7 Pro Solar", 0x5117, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://github.com/libmtp/libmtp/issues/251 */
+  { "Garmin", 0x091e, "Fenix 8 AMOLED", 0x51b8, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://github.com/libmtp/libmtp/issues/207 */
   { "Garmin", 0x091e, "Tactix 7 AMOLED", 0x51be, DEVICE_FLAGS_ANDROID_BUGS },
 
