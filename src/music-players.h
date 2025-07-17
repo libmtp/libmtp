d904a4c45cbfdfb3c2e615ec7529c18a66c88c15
Marcus Meissner <meissner@suse.de>
2025-07-17 09:26:04 +0200

    added Garmin Instinct 3 Solar, https://github.com/libmtp/libmtp/issues/309

--- src/music-players.h
+++ src/music-players.h
@@ -4140,6 +4140,8 @@
   { "Garmin", 0x091e, "Forerunner 570", 0x51de, DEVICE_FLAGS_ANDROID_BUGS },
   /* https://github.com/libmtp/libmtp/issues/303 */
   { "Garmin", 0x091e, "Vivoactive 6", 0x5211, DEVICE_FLAGS_ANDROID_BUGS },
+  /* https://github.com/libmtp/libmtp/issues/309 */
+  { "Garmin", 0x091e, "Instinct 3 Solar", 0x5297, DEVICE_FLAGS_ANDROID_BUGS },
 
   /*
    * Wacom
