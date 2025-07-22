13f058627f689ab65cb1d618748a285f7cef2316
Marcus Meissner <marcus@jet.franken.de>
2025-07-22 17:33:17 +0200

    updated savannah urls
    fixes https://github.com/libmtp/libmtp/issues/314

--- autogen.sh
+++ autogen.sh
@@ -55,10 +55,10 @@ if [ "x$WGET" != "x" ]; then
     echo "Autoupdate config.sub and config.guess (y/n)?"
     read IN
     if [ "$IN" = "y" ] || [ "$IN" = "Y" ]; then
-	wget -O tmpfile http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.guess
-	mv tmpfile config.guess
-	wget -O tmpfile http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.sub
-	mv tmpfile config.sub
+        wget -O config.guess https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess
+        wget -O config.sub https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub
+        chmod +x config.guess config.sub
+        echo "config.guess and config.sub updated"
     fi
 else
     echo "Could not autoupdate config.sub and config.guess"
