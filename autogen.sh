4777b748cf52a2873ec5a087c4487f0c4a88b8d4
Marcus Meissner <marcus@jet.franken.de>
2025-12-21 13:51:36 +0100

    add debug

--- autogen.sh
+++ autogen.sh
@@ -23,6 +23,7 @@ echo "Running libtoolize"
 (glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
 $LIBTOOLIZE --copy --force || fail
 
+echo "Running gettextize --force"
 gettextize --force
 
 # Refresh GNU autotools toolchain: aclocal autoheader
