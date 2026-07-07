libmtp/libmtp

commit 8d2f79c7adbfe452ce158028c3a4ff052f02a7c2

Add support for HumanWare Victor Stream 3 (fixes #389)
Add device entry for HumanWare Victor Stream 3 (VID=0x1c71,
PID=0xc150), an Android-based MTP device identified via mtp-detect.

src/music-players.h
@@ -4582,3 +4582,6 @@
 
   /* qemu 3.0.0 hw/usb/dev-mtp.c */
   { "QEMU", 0x46f4, "Virtual MTP", 0x0004, DEVICE_FLAG_NONE }
+
+  /* https://github.com/libmtp/libmtp/issues/389 */
+  { "HumanWare", 0x1c71, "Stream 3 (Victor Reader Stream)", 0xc150, DEVICE_FLAGS_ANDROID_BUGS }

commit 9ae5b3d84fe6d56201429b83743e3c2ce5a0e8fc

Fix the build
Fixes: 8d2f79c

src/music-players.h
@@ -4587,7 +4587,7 @@
   { "Hawkeye", 0x1d6b, "Ai15", 0x0101, DEVICE_FLAGS_ANDROID_BUGS },
 
   /* qemu 3.0.0 hw/usb/dev-mtp.c */
-  { "QEMU", 0x46f4, "Virtual MTP", 0x0004, DEVICE_FLAG_NONE }
+  { "QEMU", 0x46f4, "Virtual MTP", 0x0004, DEVICE_FLAG_NONE },
 
   /* https://github.com/libmtp/libmtp/issues/389 */
   { "HumanWare", 0x1c71, "Stream 3 (Victor Reader Stream)", 0xc150, DEVICE_FLAGS_ANDROID_BUGS }